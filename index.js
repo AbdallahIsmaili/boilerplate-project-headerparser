227bcf40f0d585e2fc67124d6c027489978d400b
AbdallahIsmaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2025-06-30 18:16:40 +0100

    Initial commit
    Set up project structure and add initial files.

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.set('trust proxy', true);
+
+app.get("/api/whoami", (req, res) => {
+
+  const ipaddress = req.ip;
+  const software = req.headers['user-agent'];
+  const language = req.headers['accept-language'];
+
+
+  res.json({ipaddress: ipaddress, language: language, software: software});
+
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
